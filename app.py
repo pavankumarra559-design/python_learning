e6f86f22ba14c532ea52c9f5cf01b9fd27c13c24
Pavan Kumar <pavankumarra559@gmail.com>
2026-06-13 20:36:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+#It is a program to check wether the number is odd or even
+i=float(input("Enter the number: "))
+if i%2==0:
+    print(i,"This number is even")
+else:
+    print(i,"This number is odd")
+
